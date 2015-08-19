641d431d067a0bc5469958013f856cbed7b088c6
Ze Li <zhanglizeyi@gmail.com>
2015-08-18 23:59:06 -0700

    Initial Commit

---
+++ Maze/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Maze
+//
+//  Created by ze li on 8/18/15.
+//  Copyright © 2015 ze li. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
